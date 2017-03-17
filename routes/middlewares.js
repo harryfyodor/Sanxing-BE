7cab5b559687c4419a67bced004eca17c122a9bf
hhk <codeforsomeone@gmail.com>
2017-03-17 11:35:14 +0800

    复用代码

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,14 +1,11 @@
+let errHandler = require('../utils/respondUtils').errHandler;
+
 function checkLogin(req, res, next) {
 	// req.session.name = "harry";
 	if(req.session.name) {
 		next();
 	} else {
-		return res.send({
-			"code":200,
-			"enmsg":"not login",
-			"cnmsg":"请重新登录",
-			"data":null
-		});
+		return errHandler(res, null, 200, "not login", "请重新登录");
 	}
 }
 
